56f8417b51be673e5f6e2c62984cb2e5daf347db
sitinurkaffah <92791203+sitinurkaffah@users.noreply.github.com>
2023-06-06 14:13:24 +0700

    Delete movie-database directory

--- movie-database/src/components/Alert/Alert.js
+++
@@ -1,9 +0,0 @@
-const Alert = (props) => {
-/**
-   * Children adalah props khusus.
-   * Berisi konten/children yang ada di dalam component.
-   */
-return <div>{props.children}</div>
-}
-
-export default Alert;
